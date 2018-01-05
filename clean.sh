0b42997891d271e56c20be417c0de0351e256a98
Anand Capur <git@anand.io>
2018-01-04 17:09:31 -0800

    asfasdf

--- clean.sh
+++ clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-snapcraft clean apache-customizations pterodactyl redis-customizations php-customizations pterodactyl-customizations mysql-customizations delay-on-failure setup-https hooks
+snapcraft clean apache-customizations pterodactyl redis-customizations php-customizations pterodactyl-customizations mysql-customizations delay-on-failure setup-https hooks composer
